c90a8137ea0ab8fb2b324647b8091a841c88c569
24160679-Merlin <24160679@itoaxaca.edu.mx>
2026-05-27 06:20:32 -0600

    V1.4 Cambio index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Truper - Herramientas Profesionales</title>
+    <title>Truper S.A - Herramientas Profesionales</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
